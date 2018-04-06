799c669e16235c29340e37d87611b0e8abeb9696
kwnspir <38087747+kwnspir@users.noreply.github.com>
2018-04-06 23:17:32 +0300

    Update electricfield.py

--- electricfield.py
+++ electricfield.py
@@ -1,26 +1,28 @@
 from vpython import *
-
-
+import math
 ke = 9e9
-
-
 scene = canvas(width = 1280, height = 720)
-
-
 positive = sphere(pos = vector(0,0,0), color = color.red, radius = 10)
-#neg = sphere(pos = vector(100,0,0), color = color.blue,radius = 10)
+
+theta=0
+fi=0
+r=10
+for theta in range(0,360,1):
+    for fi in range(0,360,1):
+        i=r*sin(math.radians(fi))*cos(math.radians(theta))
+        j=r*sin(math.radians(fi))*sin(math.radians(theta))
+        k=r*cos(math.radians(fi))
+        if(i==0 and j==0 and k==0):
+            print("ERROR_EVENT_HANDLED")
+
+        else:
+            rpos = vector(i,j,k)
+            fieldpos = 9e9/mag(rpos)**2 
+            totalfield = fieldpos 
+            field1 = arrow(pos = vector(i,j,k), axis = (rpos),length= abs(totalfield)/1000 )
 
 '''
-for i in range(-1000,1000,100):
-    for j in range(-1000,1000,100):
-        rpos = vector(i,j,0)-pos.pos
-        rneg = vector(i,j,0)-neg.pos
-        fieldpos = (-ke/(mag(rpos)**2)) * (rpos/mag(rpos))
-        fieldneg = (ke/(mag(rneg)**2)) * (rneg/mag(rneg))
-        totalfield = fieldpos + fieldneg
-        field = arrow(pos = vector(i,j,0), axis = (totalfield/mag(totalfield)),length= mag(totalfield)/5000 )
-'''
-step=200
+step=100
 
 for i in range(-1000,1000,step):
     for j in range(-1000,1000,step):
@@ -32,5 +34,5 @@ for i in range(-1000,1000,step):
                 rpos = vector(i,j,k)
                 fieldpos = 9e9/mag(rpos)**2 
                 totalfield = fieldpos 
-                field1 = arrow(pos = vector(i,j,k), axis = (rpos),length= abs(totalfield)/10000 )
-                #field2 = arrow(pos = vector(-i,j,k), axis = vector(-rpos.x,rpos.y,rpos.z),length= abs(totalfield)/10000 )
+                field1 = arrow(pos = vector(i,j,k), axis = (rpos),length= abs(totalfield)/1000 )
+'''
