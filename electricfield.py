29c69e0edb1247ab7e78a397a984ede6ee3dce0c
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2018-04-06 23:30:53 +0300

    Update electricfield.py

--- electricfield.py
+++ electricfield.py
@@ -1,25 +1,26 @@
 from vpython import *
 import math
 ke = 9e9
-scene = canvas(width = 1280, height = 720)
+scene = canvas(width = 1600, height = 900)
 positive = sphere(pos = vector(0,0,0), color = color.red, radius = 10)
 
 theta=0
 fi=0
-r=10
-for theta in range(0,360,1):
-    for fi in range(0,360,1):
-        i=r*sin(math.radians(fi))*cos(math.radians(theta))
-        j=r*sin(math.radians(fi))*sin(math.radians(theta))
-        k=r*cos(math.radians(fi))
-        if(i==0 and j==0 and k==0):
-            print("ERROR_EVENT_HANDLED")
+r=100
+for r in range(100,1000,50):
+    for theta in range(0,360,18):
+        for fi in range(0,360,18):
+            i=r*sin(math.radians(fi))*cos(math.radians(theta))
+            j=r*sin(math.radians(fi))*sin(math.radians(theta))
+            k=r*cos(math.radians(fi))
+            if(i==0 and j==0 and k==0):
+                print("ERROR_EVENT_HANDLED")
 
-        else:
-            rpos = vector(i,j,k)
-            fieldpos = 9e9/mag(rpos)**2 
-            totalfield = fieldpos 
-            field1 = arrow(pos = vector(i,j,k), axis = (rpos),length= abs(totalfield)/1000 )
+            else:
+                rpos = vector(i,j,k)
+                fieldpos = 9e9/mag(rpos)**2 
+                totalfield = fieldpos 
+                field1 = arrow(pos = vector(i,j,k), axis = (rpos),length= abs(totalfield)/20000 ,shaftwidth = 3)
 
 '''
 step=100
