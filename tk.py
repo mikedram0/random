269c4771b57312cfa360db55ccdf9e40234b5a9a
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2019-03-15 20:19:10 +0200

    Update tk.py

--- tk.py
+++ tk.py
@@ -39,26 +39,25 @@ class coll_obj:
 			self.y = self.radius
 			self.vy *= -1
 		for circle in list1:
-			try:
-				vec = math.sqrt((circle.x-self.x)**2+(circle.y-self.y)**2)
-				if vec <= self.radius + circle.radius:
-					u1 = math.sqrt((circle.vx)**2 + (circle.vy)**2)
-					u2 = math.sqrt((self.vx)**2 + (self.vy)**2)
-					phi = math.acos((circle.x-self.x)/vec)
-					heta1 = math.acos(circle.vx/u1)
-					theta2 = math.acos(self.vx/u2)
+			if circle == self:
+				continue
+			vec = math.sqrt((circle.x-self.x)**2+(circle.y-self.y)**2)
+			if vec <= self.radius + circle.radius:
+				u1 = math.sqrt((circle.vx)**2 + (circle.vy)**2)
+				u2 = math.sqrt((self.vx)**2 + (self.vy)**2)
+				phi = math.acos((circle.x-self.x)/vec)
+				theta1 = math.acos(circle.vx/u1)
+				theta2 = math.acos(self.vx/u2)
 
-					circle.vx = ((2*u2*cos(theta2-phi))/2)*math.cos(phi) + u1*math.sin(theta1-phi)*math.sin(phi)
-					circle.vy = ((2*u2*cos(theta2-phi))/2)*math.sin(phi) + u1*math.sin(theta1-phi)*math.cos(phi)
+				circle.vx = ((2*u2*math.cos(theta2-phi))/2)*math.cos(phi) + u1*math.sin(theta1-phi)*math.sin(phi)
+				circle.vy = ((2*u2*math.cos(theta2-phi))/2)*math.sin(phi) + u1*math.sin(theta1-phi)*math.cos(phi)
 
-					self.vx = ((2*u1*cos(theta1-phi))/2)*math.cos(phi) + u2*math.sin(theta2-phi)*math.sin(phi)
-					self.vy = ((2*u1*cos(theta1-phi))/2)*math.sin(phi) + u2*math.sin(theta2-phi)*math.cos(phi)
-			except:
-  				print("An error occurred and handled succesfully.")
-            
+				self.vx = ((2*u1*math.cos(theta1-phi))/2)*math.cos(phi) + u2*math.sin(theta2-phi)*math.sin(phi)
+				self.vy = ((2*u1*math.cos(theta1-phi))/2)*math.sin(phi) + u2*math.sin(theta2-phi)*math.cos(phi)
+			
 
 
-for circle in range(10):
+for circle in range(2):
     circle = coll_obj(random.randint(25,50),random.randint(0,300),random.randint(0,250),random.randint(-20,20),random.randint(-20,20))
     circle.draw()
     list1.append(circle)
