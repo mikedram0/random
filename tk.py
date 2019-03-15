3ef4843c19be337a4f1296813806727b44fa72f0
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2019-03-15 16:50:37 +0200

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,17 +1,24 @@
 import tkinter as tk
+import random
 
 top = tk.Tk()
-canv=tk.Canvas(top, bg="black", height=250, width=300)
+canv=tk.Canvas(top, bg="white", height=250, width=300)
 
 class coll_obj:
 	def __init__(self,radius,x,y):
 		self.radius=radius
 		self.x=x
 		self.y=y
-
 	def create(self):
 		c = canv.create_oval(self.x-self.radius,self.y-self.radius,self.x+self.radius,self.y+self.radius)
 
+
+c1 = coll_obj(50,150,125)
+
+for circle in range(10):
+    circle = coll_obj(random.randint(25,50),random.randint(0,300),random.randint(0,250))
+    circle.create()
+                      
 
 canv.pack()
 top.mainloop()
