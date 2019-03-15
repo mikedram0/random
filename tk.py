34aa04c908aeec057052f81a6eaae625eef8dc69
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2019-03-15 18:27:46 +0200

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 import random
 import time
+import math
 canvh=720
 canvw=1280
 top = tk.Tk()
 canv=tk.Canvas(top, bg="black", height=canvh, width=canvw)
 list1=[]
 
+
+colors = ["white","blue","red","yellow","orange"]
 
 class coll_obj:
     def __init__(self,radius,x,y,vx,vy):
@@ -17,7 +20,7 @@ class coll_obj:
         self.vy=vy
 	
     def draw(self):
-        c = canv.create_oval(self.x-self.radius,self.y-self.radius,self.x+self.radius,self.y+self.radius,fill="white")
+        self.id = canv.create_oval(self.x-self.radius,self.y-self.radius,self.x+self.radius,self.y+self.radius,fill="white")
     def move(self):
         self.x += self.vx
         self.y += self.vy
@@ -34,17 +37,25 @@ class coll_obj:
         if self.y < self.radius:
             self.y = self.radius
             self.vy *= -1
+        for circle in list1:
+            if math.sqrt((circle.x-self.x)**2+(circle.y-self.y)**2)<= self.radius + circle.radius:
+                temp = self.x
+                self.x = circle.x
+                circle.x = temp
+                temp = self.y
+                self.y = circle.y
+                circle.y = temp
             
 
 
-for circle in range(3):
+for circle in range(10):
     circle = coll_obj(random.randint(25,50),random.randint(0,300),random.randint(0,250),random.randint(-20,20),random.randint(-20,20))
     circle.draw()
     list1.append(circle)
 
 
-i=0
-while(i<1000):
+
+while(1):
     canv.delete("all")
     for circle in list1:
         circle.move()
@@ -53,5 +64,4 @@ while(i<1000):
     time.sleep(0.005)
     canv.pack()
     top.update()
-    i=i+1
     #top.mainloop()
