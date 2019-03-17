fd597370acc223b68c4edfd22be1bf2dd1d93b78
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-17 19:12:32 +0200

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,21 +1,42 @@
-import pygame
-
+import sys, pygame
 pygame.init()
 
-height=900
-widht=1600
+size = width, height = 800, 600
+black = 0, 0, 0
 
-gameDisplay=pygame.display.set_mode((widht,height))
-pygame.display.set_caption("Asteroids")
+screen = pygame.display.set_mode(size)
 
-play=True
+ship = pygame.image.load("starship.png")
+shiprect = ship.get_rect()
+x=width/2
+y=height/2
+vx=0.02
+vy=0.03
 
-while play==True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT():
-			play=False
-		pygame.display.update()
-		clock.tick(60)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
-pygame.quit()
-quit()
+
+    x += vx
+    y += vy
+
+    if x>width-shiprect[2]:
+        x = width - shiprect[2]
+        vx *= -1
+    if x<0:
+        x = 0
+        vx *= -1
+    if y>height-shiprect[3]:
+        y = height - shiprect[3]
+        vy *= -1
+    if y<0:
+        y = 0
+        vy *= -1
+
+    ship=pygame.transform.rotate(ship,10)
+
+
+    screen.fill(black)
+    screen.blit(ship, (x,y))
+    pygame.display.flip()
