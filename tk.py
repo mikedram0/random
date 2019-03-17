8e2cdb99687652465f145f6d209200cc83aa3860
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-17 17:00:11 +0200

    Update tk.py

--- tk.py
+++ tk.py
@@ -3,17 +3,6 @@ import random
 import time
 import math
 import threading as thr
-#canvh=900
-#canvw=1600
-
-#top = tk.Tk()
-#canv=tk.Canvas(top, bg="black", height=canvh, width=canvw)
-#canv.pack()
-#fps = 60
-#list1=[]
-
-
-
 
 class coll_obj:
 	def __init__(self,radius,x,y,vx,vy):
@@ -104,65 +93,48 @@ class coll_obj:
 
 
 class game():
-	def __init__(self,canvh,canvw,starting_balls,fps,list1,main_frame,canv):
+	def __init__(self):
 		self.canvh=900
 		self.canvw=1600
 		self.starting_balls=20
 		self.fps=60
 		self.list1=[]
 		self.main_frame=tk.Tk()
-		self.canv=self.tk.Canvas(self.main_frame, bg="black", height=self.canvh, width=self.canvw)
+		self.canv=tk.Canvas(self.main_frame, bg="black", height=self.canvh, width=self.canvw)
 		self.canv.pack()
 
 	def main(self):
-		pass
+		self.canv.delete("ball")
+		self.main_frame.bind("<Button-3>", self.create_balls)
+		self.main_frame.bind("<B1-Motion>", self.btn_move)
+		#self.main_frame.bind("<ButtonRelease1>", brelease)
+		for circle in self.list1:
+			circle.move()
+			circle.bordercollision()
+			circle.btbcollision()
+			circle.draw()
+		time.sleep(1/self.fps)
+		self.main_frame.update()
 
 	def create_balls(self):
 		for circle in range(3):
-    		circle = coll_obj(random.randint(10,20),random.randint(0,canvw),random.randint(0,canvh),random.randint(-5,5),random.randint(-5,5))
-    		list1.append(circle)
+			circle = coll_obj(random.randint(10,20),random.randint(0,canvw),random.randint(0,canvh),random.randint(-5,5),random.randint(-5,5))
+			list1.append(circle)
 
-    def btn_move(self):
-    	pass
+	def btn_move(self):
+		try:
+			if self.canv.coords("current")!=[]:
+				print(self.canv.coords("current"))
+			self.tmpl=self.canv.coords("current")
+			self.canv.create_line((self.tmpl[2]+self.tmpl[0])/2,(self.tmpl[1]+self.tmpl[3])/2,self.event.x,self.event.y,fill="cyan",width = 2)
+		except:
+			pass
 
-    def btn_create(self):
-    	circle = coll_obj(random.randint(10,20),event.x,event.y,0,0)
+	def btn_create(self):
+		circle = coll_obj(random.randint(10,20),event.x,event.y,0,0)
 		circle.draw()
 		self.list1.append(circle)
-
-
-def main():
-	canv.delete("ball")
-	top.bind("<Button-3>", btn_input)
-	top.bind("<B1-Motion>", btn_mot)
-	#top.bind("<ButtonRelease1>", brelease)
-	for circle in list1:
-		circle.move()
-		circle.bordercollision()
-		circle.btbcollision()
-		circle.draw()
-	time.sleep(1/fps)
-	top.update()
-
-
-
-
-
-def btn_mot(event):
-	global list1
-	tmpl=[0,0,0,0]
-	try:
-		if canv.coords("current")!=[]:
-			print(canv.coords("current"))
-		tmpl=canv.coords("current")
-		canv.create_line((tmpl[2]+tmpl[0])/2,(tmpl[1]+tmpl[3])/2,event.x,event.y,fill="cyan",width = 2)
-	except:
-		pass
+	
 
 game1=game()
-#while 1:
-#	main()
-#if __name__ == '__main__':
-#    for j in range(10000):
-#        t1 = thr.Thread(target=main())
-#        t1.start()
+game1.main()
