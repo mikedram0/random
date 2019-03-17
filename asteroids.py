a5518731c0f0519c6b78f93a9fd075da251c819d
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-17 18:20:17 +0200

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,2 +1,9 @@
 import pygame
 pygame.init()
+gameDisplay=pygame.display.set_mode((1600,900))
+pygame.display.set_caption("Asteroids")
+loop=1
+while loop==1:
+	for i in range(100):
+		print("hey")
+	loop=0
