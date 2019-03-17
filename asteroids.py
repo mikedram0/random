86e1c9456eeb85d93289c5891e2da72be08ad0b3
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2019-03-17 19:08:01 +0200

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -7,6 +7,7 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 ship = pygame.image.load("starship.png")
+shiprect = ship.get_rect()
 x=width/2
 y=height/2
 vx=0.02
@@ -16,10 +17,23 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+
     x += vx
     y += vy
 
-    #if x>width:
+    if x>width-shiprect[2]:
+        x = width - shiprect[2]
+        vx *= -1
+    if x<0:
+        x = 0
+        vx *= -1
+    if y>height-shiprect[3]:
+        y = height - shiprect[3]
+        vy *= -1
+    if y<0:
+        y = 0
+        vy *= -1
+
 
     screen.fill(black)
     screen.blit(ship, (x,y))
