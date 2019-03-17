e919bbf5912c69e03cb64087b9c6a0b45b677963
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-17 18:58:29 +0200

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -1,1 +1,21 @@
+import pygame
 
+pygame.init()
+
+height=900
+widht=1600
+
+gameDisplay=pygame.display.set_mode((widht,height))
+pygame.display.set_caption("Asteroids")
+
+play=True
+
+while play==True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT():
+			play=False
+		pygame.display.update()
+		clock.tick(60)
+
+pygame.quit()
+quit()
