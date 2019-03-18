31a362d9ac83fa285768ab2e291ea61766b232a4
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-18 19:02:22 +0200

    Update asteroids.py

--- asteroids.py
+++ asteroids.py
@@ -4,6 +4,7 @@ import time
 import math
 import random
 
+
 pygame.init()
 
 size = width, height = 1920, 1080
@@ -13,9 +14,17 @@ black = 0, 0, 0
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 #screen = pygame.display.set_mode((width, height))
 
+pygame.mixer.music.load('beat.mp3')
+pygame.mixer.music.play(-1)
 ship = pygame.image.load("starship.png")
 bullet_img = pygame.image.load("bullet.png")
-asteroid_img = pygame.image.load("asteroid_img.png")
+asteroid_small = pygame.image.load("small.png")
+asteroid_medium = pygame.image.load("medium.png")
+asteroid_large = pygame.image.load("large.png")
+
+astersize = {1:asteroid_small,2:asteroid_medium,3:asteroid_large}
+
+
 shiprect = ship.get_rect()
 x=width/2
 y=height/2
@@ -28,16 +37,44 @@ bullets=[]
 asteroids_list=[]
 
 class asteroid:
-	def __init__(self):
-		self.aposx=random.randint(0,width)
-		self.aposy=random.randint(0,height)
-		self.avx=random.randint(-5,5)
-		self.avy=random.randint(-5,5)
+	def __init__(self,x,y,size):
+		self.aposx=x
+		self.aposy=y
+		self.avx=random.randint(-1,1)
+		self.avy=random.randint(-1,1)
+		
+		self.size = size
+		self.image = astersize[self.size]
+		self.bbox = self.image.get_rect()
 
-	def draw(self):
+	def collisiondetect(self):
+		for bullet in bullets:
+			if bullet.bposx > self.aposx and bullet.bposx < self.aposx + self.bbox[2] and bullet.bposy > self.aposy and bullet.bposy < self.aposy + self.bbox[3]:
+				self.collision()
+				bullets.remove(bullet)
+
+
+	def collision(self):
+		asteroids_list.remove(self)
+		if self.size > 1:
+			asteroids_list.append(asteroid(self.aposx,self.aposy,self.size-1))
+			asteroids_list.append(asteroid(self.aposx,self.aposy,self.size-1))
+
+	def move(self):
 		self.aposx=self.aposx+self.avx
-		self.aposx=self.aposx+self.avy
-		screen.blit(asteroid_img, (self.aposx,self.aposy))
+		self.aposy=self.aposy+self.avy
+		if self.aposx>width:
+			self.aposx = 0
+		if self.aposx<0:
+			self.aposx = width
+		if self.aposy>height:
+			self.aposy = 0 
+		if self.aposy<0:
+			self.aposy = height
+		
+	def draw(self):
+		screen.blit(self.image, (self.aposx,self.aposy))
+
 
 
 class Bullet:
@@ -46,28 +83,34 @@ class Bullet:
 		self.bposy=bposy
 		self.bvx=math.sin(angle*3.1415/180 +3.1415) 
 		self.bvy=math.cos(angle*3.1415/180+3.1415)
-        # self.fire()
+	   # self.fire()
 
-	def fire(self):
-        
-		print("test")
+	def checkdelete(self):
+		if self.bposx >width or self.bposx<0 or self.bposy > height or self.bposy < 0:
+			bullets.remove(self)
 
 	def draw(self):
+		screen.blit(bullet_img, (self.bposx,self.bposy))
+		
+	def move(self):
 		self.bposx=self.bposx+self.bvx*20
 		self.bposy=self.bposy+self.bvy*20
-		screen.blit(bullet_img, (self.bposx,self.bposy))
+
+	for i in range(10):
+		asteroids_list.append(asteroid(random.randint(0,width),random.randint(0,height),random.randint(1,3)   ))
+
 
 while 1:
 	time.sleep(1/FPS)
 	angle=angle+anglev
-	asteroids_list.append(asteroid())
-    #print(1)
+	
+	#print(1)
 	for event in pygame.event.get():
 		#print(2)
 		if event.type == pygame.QUIT: 
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
-            
+			
 			if event.key==pygame.K_ESCAPE:
 				sys.exit()
 
@@ -76,7 +119,8 @@ while 1:
 
 			if event.key==pygame.K_SPACE:
 				bullets.append(Bullet(x,y))
-                
+				print("piew")
+				
 
 			if event.key==pygame.K_d:
 				anglev=anglev-1
@@ -98,8 +142,8 @@ while 1:
 		y = 0 - shiprect[3]
 	if y<0-shiprect[3]:
 		y = height
-        
-
+		
+	#print(bullets)
 	old=shiprect.center
 	new_ship=pygame.transform.rotate(ship,angle)
 	shiprect = new_ship.get_rect()
@@ -111,10 +155,15 @@ while 1:
 	screen.fill(black)
 
 	for aster in asteroids_list:
+		aster.collisiondetect()
+		aster.move()
 		aster.draw()
 
 	for bullet in bullets:
-		bullet.draw()
+		bullet.checkdelete()
+		if bullets.count(bullet):
+			bullet.move()
+			bullet.draw()
 
 
 	screen.blit(new_ship, (x,y))
