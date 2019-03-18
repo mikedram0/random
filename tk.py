69dd89c386077a911e6b66ee379d70df1202c2e7
kwnspir <38087747+kwnspir@users.noreply.github.com>
2019-03-18 17:11:30 +0200

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,151 +1,96 @@
-import tkinter as tk 
-import random
+import sys
+import pygame
 import time
 import math
-import threading as thr
-canvh=480
-canvw=640
+pygame.init()
 
-top = tk.Tk()
-canv=tk.Canvas(top, bg="black", height=canvh, width=canvw)
-canv.pack()
-fps = 60
-list1=[]
+size = width, height = 1600, 900
+black = 0, 0, 0
 
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((width, height))
 
+ship = pygame.image.load("starship.png")
+bullet_img = pygame.image.load("bullet.png")
+shiprect = ship.get_rect()
+x=width/2
+y=height/2
+vx=0
+vy=0
+angle=0
+anglev=0
+FPS=60
+bullets=[]
 
+class bullet:
+	def __init__(self,bposx,bposy,bvx,bvy):
+		self.bposx=bposx
+		self.bposy=bposy
+		self.bvx=bvx
+		self.bvy=bvy
+		self.bullet_rect=self.bullet_rect.get_rect()
+		self.fire()
 
-class coll_obj:
-	def __init__(self,radius,x,y,vx,vy):
-		self.radius=radius
-		self.x=x
-		self.y=y
-		self.vx=vx
-		self.vy=vy
-		self.mass = self.radius**2
-		self.draw()
-		#self.id.itemconfig(tags="ball")
-	
-	def draw(self):	
-
-		self.id = canv.create_oval(self.x-self.radius,self.y-self.radius,self.x+self.radius,self.y+self.radius,fill="white",tags="ball")
-
-	def move(self):
-		self.x += self.vx
-		self.y += self.vy
-		#canv.move(self.id,self.vx,self.vy)
-	def bordercollision(self):
-		if self.x  > canvw - self.radius:
-			self.x = canvw - self.radius 
-			self.vx *= -1
-		if self.x < self.radius:
-			self.x = self.radius
-			self.vx *= -1
-		if self.y > canvh - self.radius:
-			self.y = canvh - self.radius
-			self.vy *= -1
-		if self.y < self.radius:
-			self.y = self.radius
-			self.vy *= -1
-	def btbcollision(self):
-		for circle in list1:
-			if circle == self:
-				continue
-			dist = math.sqrt((circle.x-self.x)**2+(circle.y-self.y)**2)
-			if dist <= self.radius + circle.radius:
-
-				overlap = self.radius + circle.radius - dist
-
-				#Calculating the normal vector and normalizing it
-
-				nx = (circle.x - self.x)/dist
-				ny = (circle.y - self.y)/dist
-
-				#Calculating the tangential vector 
-
-				tx = ny
-				ty = -1*nx
-
-				#Displacing the balls along the parallel axis in case of overlap
-
-				self.x -= nx * overlap/2
-				self.y -= ny * overlap/2
-
-				circle.x += nx* overlap/2
-				circle.y += ny*overlap/2
-
-
-				# Here I will use the collison "coordinates" , by breaking the velocity into a tangential and parralell part
-
-
-				#Calculating the tangential component of velocity , which are not affected by the collision
-
-				vtan1 = self.vx*tx +self.vy*ty
-				vtan2 = circle.vx*tx + circle.vy*ty
-
-				#Calculating the parralell velocity before the colision
-
-				vpar1b = self.vx * nx + self.vy * ny
-				vpar2b = circle.vx * nx + circle.vy * ny
-
-
-				#Calculating the parallell velocities after the collision , using the elastic collision formula
-
-				vpar1a = ((self.mass - circle.mass)*vpar1b + 2*circle.mass*vpar2b)/(self.mass + circle.mass)
-				vpar2a = (2*self.mass*vpar1b + (circle.mass - self.mass)*vpar2b)/(self.mass + circle.mass)
-
-
-				#Setting the velocities
-
-				self.vx = vtan1*tx + nx * vpar1a
-				self.vy = vtan1*ty + ny * vpar1a
-
-				circle.vx = vtan2*tx + nx * vpar2a
-				circle.vy = vtan2*ty + ny * vpar2a
-
-for circle in range(5):
-    circle = coll_obj(random.randint(10,20),random.randint(0,canvw),random.randint(0,canvh),random.randint(-5,5),random.randint(-5,5))
-    list1.append(circle)
-
-
-def main():
-	canv.delete("all")
-	top.bind("<Button-3>", btn_input)
-	top.bind("<1>", btn_mot)
-	#top.bind("<ButtonRelease1>", brelease)
-	for circle in list1:
-		circle.move()
-		circle.bordercollision()
-		circle.btbcollision()
-		circle.draw()
-	time.sleep(1/fps)
-	top.update()
-
-
-
-
-
-def btn_mot(event):
-	global list1
-	try:
-		if canv.coords("current")!=[]:
-			print(canv.coords("current"))
-		tmpl=canv.coords("current")
-		canv.create_line((tmpl[2]+tmpl[0])/2,(tmpl[1]+tmpl[3])/2,event.x,event.y,fill="red",width = 4)
-	except:
-		pass
-
-def btn_input(event):
-	global list1
-	circle = coll_obj(random.randint(10,20),event.x,event.y,0,0)
-	circle.draw()
-	list1.append(circle)
-
+	def fire(self):
+		self.bposx=self.bposx+self.bvx
+		self.bposy=self.bposy+self.bvy
+		print("test")
+		screen.blit(bullet_img,(int(self.bposx),int(self.bposy)))
+		print(int(self.bposx),int(self.bposy))
 
 
 while 1:
-	main()
-#if __name__ == '__main__':
-#    for j in range(10000):
-#        t1 = thr.Thread(target=main())
-#        t1.start()
+	time.sleep(1/FPS)
+	angle=angle+anglev
+    #print(1)
+	for event in pygame.event.get():
+		#print(2)
+		if event.type == pygame.QUIT: 
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+            
+			if event.key==pygame.K_ESCAPE:
+				sys.exit()
+
+			if event.key==pygame.K_a:
+				anglev=anglev+1
+
+			if event.key==pygame.K_s:
+				bullet1=bullet(x,y,vx,vy)
+				bullets.append(bullet1)
+				print("piew")
+                
+
+			if event.key==pygame.K_d:
+				anglev=anglev-1
+
+
+			if event.key == pygame.K_w:
+				vx += math.sin(angle*3.1415/180 +3.1415) 
+				vy += math.cos(angle*3.1415/180+3.1415) 
+
+
+	x += vx
+	y += vy
+
+	if x-shiprect[2]>width:
+		x = 0 - shiprect[1]
+	if x+shiprect[2]<0:
+		x = width
+	if y>height:
+		y = 0 - shiprect[3]
+	if y<0-shiprect[3]:
+		y = height
+        
+
+	old=shiprect.center
+	new_ship=pygame.transform.rotate(ship,angle)
+	shiprect = new_ship.get_rect()
+	shiprect.center=old
+
+	screen.fill(black)
+	screen.blit(new_ship, (x,y))
+	pygame.display.update()
+
+pygame.quit()
+quit()
