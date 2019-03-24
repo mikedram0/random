4e2919f51b6b3647c80f8d1accfcb0dfb70c769c
mikedram0 <38087726+mikedram0@users.noreply.github.com>
2019-03-24 11:54:32 +0200

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,31 +1,60 @@
-#include "pch.h"
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int i = 0;
-	unsigned long long int N = 999999;
-	while (1) {
-		unsigned long long result = 1;
+	ifstream infile; 
+	infile.open("outc.txt",ios::in);
+	
+	string line;
+	int stepmax = 0;
 
-		while (N != 0) {
-			unsigned long long digit = N % 10;
-			N = N / 10;
-			result = result * digit;
+	while(getline(infile,line)) {
+    
+    	//string line;
+		int i = 0;
+
+
+		
+		unsigned long long int N = stoi(line);
+		unsigned long long int n = N;
+
+		while (1) {
+			unsigned long long int result = 1;
+
+			while (N != 0) {
+				unsigned long long int digit = N % 10;
+				N = N / 10;
+				result = result * digit;
+			}
+			//cout << result << endl;
+			i++;
+
+			if (result < 10) {
+				break;
+			}
+
+			N = result;
+	}
+		//cout << "TOTAL STEPS: " << i << endl;
+
+		
+
+		if(i > stepmax){
+
+			stepmax = i;
+			cout << "New record: " << i << " with number: " << n << endl;
+
 		}
-		cout << result << endl;
-		i++;
 
-		if (result < 10) {
-			break;
-		}
 
-		N = result;
-}
-	cout << "TOTAL STEPS: " << i;
+	}
+
+	infile.close();
 
 	return 0;
 }
